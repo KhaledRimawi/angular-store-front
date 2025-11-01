d6810cf8b03f1ea1cd47d40d569670ca03545653
AbdalrhmanJuber <1211769@student.birzeit.edu>
2025-11-01 22:49:16 +0200

    add: adding

---
+++ src/app/app-module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { App } from './app';
+
+
+
+@NgModule({
+  declarations: [App],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppModule { }
